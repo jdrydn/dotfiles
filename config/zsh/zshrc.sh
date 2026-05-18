3a706d72836566629b7061aedcd533389a154914
jdrydn <james@jdrydn.com>
2026-05-18 22:28:20 +0100

    fix: Removed unused file

--- config/zsh/zshrc.sh
+++
@@ -1,6 +0,0 @@
-DOTFILES_ZSH_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "DOTFILES_ZSH_DIR: $DOTFILES_ZSH_DIR"
-
-source $DOTFILES_ZSH_DIR/01-ohmyzsh.sh
-source $DOTFILES_ZSH_DIR/02-exports.sh
-source $DOTFILES_ZSH_DIR/03-packages.sh
