cd4e6e2a7bcc0c392a63e593bc20df5f24fe4dac
James Dryden <james@jdrydn.com>
2026-06-06 21:37:58 +0100

    refactor: Moving Claude config to `dotclaude`
    @link https://github.com/jdrydn/dotclaude

--- init/03-setup.sh
+++ init/03-setup.sh
@@ -20,19 +20,5 @@ symlink_file "$DOTFILES_DIR/config/git/gitconfig" "$HOME/.gitconfig"
 # vimrc
 symlink_file "$DOTFILES_DIR/config/vimrc" "$HOME/.vimrc"
 
-if [[ "$DOTFILES_PROFILE" == "personal" ]]; then
-  printf "\n✻ Claude\n"
-
-  # CLAUDE files
-  mkdir -p "$HOME/.claude"
-  symlink_file "$DOTFILES_DIR/config/claude/CLAUDE.md" "$HOME/.claude/CLAUDE.md"
-  symlink_file "$DOTFILES_DIR/config/claude/settings.json" "$HOME/.claude/settings.json"
-  symlink_file "$DOTFILES_DIR/config/claude/statusline-command.sh" "$HOME/.claude/statusline-command.sh"
-  # symlink_dir  "$DOTFILES_DIR/config/claude/agents" "$HOME/.claude/agents"
-  symlink_dir  "$DOTFILES_DIR/config/claude/skills" "$HOME/.claude/skills"
-else
-  log_msg "INFO" "Skipping Claude config (profile: $DOTFILES_PROFILE)"
-fi
-
 unset GITCONFIG_PATH
 unset ZSHRC_PATH
